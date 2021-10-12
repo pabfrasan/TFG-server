9268f491183c20893e60f4a8e89d09c9a85a3a9f
Pablo Franco Sánchez <56022165+pabfrasan@users.noreply.github.com>
2021-10-12 11:28:49 +0200

    Set up

---
+++ TFG-server/src/main/java/us/tfg/TfgServerApplication.java
@@ -0,0 +1,13 @@
+package us.tfg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TfgServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TfgServerApplication.class, args);
+	}
+
+}
---
+++ TFG-server/src/test/java/us/tfg/TfgServerApplicationTests.java
@@ -0,0 +1,13 @@
+package us.tfg;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TfgServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
